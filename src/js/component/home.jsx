f9227a91956f61e9a8018570affb3ffcb1886ea8
AngeZambrano <129020174+AngeZambrano@users.noreply.github.com>
2023-06-28 01:27:13 +0000

    First Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [tarea, setTarea] = useState("");
+	const [listaTareas, setListaTareas] = useState([])
+
+	const handleTarea = (e) => {
+		setTarea(e.target.value);
+		console.log(tarea);
+	}
+
+	const handleSubmit = (e) => {
+		if (e.key === 'Enter') {
+			setListaTareas(listaTareas.concat(tarea));
+			setTarea("")
+			e.preventDefault();
+		}
+	}
+
+	const deleteTask = (item) => {
+		const newArr = listaTareas.filter((param, i) => i != item);
+		setListaTareas(newArr);
+	}
+
+	const tareaHTML = listaTareas.map((task, index)=> {
+		return (
+			<li key={index}>
+				{task}
+				<span onClick={() => deleteTask(index)}>x</span>
+			</li>
+		)
+	}) 
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<form>
+				<h1>TO DO LIST</h1>
+				<input type="text" placeholder="What needs to be done?" onChange={handleTarea} value={tarea} onKeyDown={handleSubmit}/>
+				<div>
+					<ul>{tareaHTML}</ul>
+				</div>
+			</form>
 		</div>
 	);
 };
